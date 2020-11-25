6933903fec1823a10d7e2d07e16b146f5171abfd
Yuri <yurmezence@gmail.com>
2020-11-26 01:31:45 +0300

    ajax likes

--- src/images/views.py
+++ src/images/views.py
@@ -1,7 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 
+from common.decorators import ajax_required
 from images.models import Image
 from .forms import ImageCreateForm
 
@@ -11,7 +14,6 @@ def image_create(request):
     if request.method == 'POST':
         form = ImageCreateForm(data=request.POST)
         if form.is_valid():
-
             cd = form.cleaned_data
             new_item = form.save(commit=False)
 
@@ -29,3 +31,22 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html',
                   {'section': 'images', 'image': image})
+
+
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+                return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ok'})
